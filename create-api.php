c1b56b887bcbfca5f0e4bd6adcb15f4162e40a21
Ghassan Fares <gfares@gmail.com>
2022-09-13 09:43:39 +0300

    another edit 3

--- create-api.php
+++ create-api.php
@@ -1,17 +1,17 @@
 <!-------- Check if Palindrome -------->
 <?php
 if isset($_GET['submit']){
-$str = $_GET["str"]
+    $str = $_GET["str"]
 
-function Palindrome($string){ 
+    function Palindrome($string){ 
     if (strrev($string) == $string){ 
         $x = "Palindrome"; 
     }
     else{
         $x = "Not Palindrome";
     }
-}
-echo jason_encode($x);
+    }
+    echo jason_encode($x);
 }
 ?>
 
@@ -20,12 +20,12 @@ echo jason_encode($x);
 <!---------- a^3 + b*c - a/b ----------->
 <?php
 if isset($_POST['submit']){
-$a = $_POST["a"]
-$b = $_POST["b"]
-$c = $_POST["c"]
+    $a = $_POST["a"]
+    $b = $_POST["b"]
+    $c = $_POST["c"]
 
-$x = ($a ** 3) + ($b * $c) + ($a / $b);
-echo jason_encode($x);
+    $x = ($a ** 3) + ($b * $c) + ($a / $b);
+    echo jason_encode($x);
 }
 ?>
 
@@ -33,20 +33,20 @@ echo jason_encode($x);
 <!----------- Check Password ------------>
 <?php
 if isset($_POST['submit']){
-  $password = $_POST['password'];
-  
-  // Validate password strength
-  $uppercase = preg_match('@[A-Z]@', $password);
-  $lowercase = preg_match('@[a-z]@', $password);
-  $number    = preg_match('@[0-9]@', $password);
-  $specialchars = preg_match('@[^\w]@', $password);
-  
-  if(!$uppercase || !$lowercase || !$number || !$specialchars || strlen($password) < 8) {
-    echo 'Not Strong';
-  }
-  else{
-    echo 'Strong';
-  } 
+    $password = $_POST['password'];
+
+    // Validate password strength
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $number    = preg_match('@[0-9]@', $password);
+    $specialchars = preg_match('@[^\w]@', $password);
+
+    if(!$uppercase || !$lowercase || !$number || !$specialchars || strlen($password) < 8) {
+        echo 'Not Strong';
+    }
+    else{
+        echo 'Strong';
+    } 
 }
 ?>
 
@@ -54,13 +54,13 @@ if isset($_POST['submit']){
 <!----------- Days till Christmas ------------>
 <?php
 if isset($_GET['submit']){
-$timeXmas = $_GET["timeXmas"]
-date_default_timezone_set('Asia/Beirut');
+    $timeXmas = $_GET["timeXmas"]
+    date_default_timezone_set('Asia/Beirut');
 
-$christmasDay = strtotime('December 25');
-$time = time();
-$days = ceil(($christmasDay - $time)/60/60/24);
+    $christmasDay = strtotime('December 25');
+    $time = time();
+    $days = ceil(($christmasDay - $time)/60/60/24);
 
-echo jason_encode($days);
+    echo jason_encode($days);
 }
 ?>
